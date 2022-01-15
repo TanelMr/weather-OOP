81bf0358e0917da09f4deaf6767a8a4325f90f98
Tanel Märjama <tanel.marjama@khk.ee>
2022-01-15 21:43:15 +0200

    Get weather data

---
+++ Classes/Weather.js
@@ -0,0 +1,11 @@
+class Weather {
+    constructor(city) {
+        this.city = city;
+        this.key ="8b0d1e31e286ec235fba8e3a8345ece8";
+    }
+    async getWeather(){
+        const response = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.key}`)
+        const responseData = await response.json();
+        return responseData;
+    }
+}
---
+++ app.js
@@ -0,0 +1,11 @@
+// objects
+const weather = new Weather("Tartu")
+
+//get city weather
+function getWeather(){
+    weather.getWeather();
+    then.(data => {
+
+    })
+        .catch(error => console.log(error))
+}
