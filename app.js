38295a9cc74bc770d8bcecfcc0ad8d8a92e23a22
Tanel Märjama <tanel.marjama@khk.ee>
2022-01-16 13:11:57 +0200

    Change weather data from API

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-// objects
+//objects
 const weather = new Weather("Tartu");
 const ui = new UI ();
 
+//events
+const form = document.querySelector("#change-city");
+form.addEventListener("submit", changeWeather)
+
+//change weather
+function changeWeather(event){
+    const city = document.querySelector("#city-name").value;
+    weather.changeCity(city);
+    getWeather();
+    document.querySelector("#city-name").value = "";
+    event.preventDefault();
+}
+
 //get city weather
 function getWeather(){
-    weather.getWeather();
-    then.(data => {
+    weather.getWeather()
+    .then (data => {
     ui.drawWeather(data);
     })
         .catch(error => console.log(error))
 }
+
+getWeather();
