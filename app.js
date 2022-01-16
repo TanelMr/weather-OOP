f37937f31855ed75ec0eea23e3e5ac6dfc80f5da
Tanel Märjama <tanel.marjama@khk.ee>
2022-01-16 13:41:10 +0200

    Get and set city from LS

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 //objects
-const weather = new Weather("Tartu");
+const ls = new LS();
+//init city
+const initCity = ls.getCity();
+const weather = new Weather(initCity);
 const ui = new UI ();
 
 //events
 const form = document.querySelector("#change-city");
-form.addEventListener("submit", changeWeather)
+form.addEventListener("submit", changeWeather);
 
 //change weather
 function changeWeather(event){
     const city = document.querySelector("#city-name").value;
     weather.changeCity(city);
+    ls.getCity(city);
     getWeather();
     document.querySelector("#city-name").value = "";
     event.preventDefault();
@@ -21,7 +25,7 @@ function getWeather(){
     .then (data => {
     ui.drawWeather(data);
     })
-        .catch(error => console.log(error))
+        .catch(error => console.log(error));
 }
 
 getWeather();
