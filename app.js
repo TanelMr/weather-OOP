724a6a4f19d6962dc54db5a33fa7dfc65e70c4ae
Tanel Märjama <tanel.marjama@khk.ee>
2022-01-16 12:20:06 +0200

    Display weather data from API

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 // objects
-const weather = new Weather("Tartu")
+const weather = new Weather("Tartu");
+const ui = new UI ();
 
 //get city weather
 function getWeather(){
     weather.getWeather();
     then.(data => {
-
+    ui.drawWeather(data);
     })
         .catch(error => console.log(error))
 }
